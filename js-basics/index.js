6c4e5d3c8c168e203baa711299bfaf786ed0cdc8
biche1 <bichechi101@gmail.com>
2023-10-12 14:10:11 +0100

    returning max

--- js-basics/index.js
+++ js-basics/index.js
@@ -9,9 +9,15 @@ b = c;
 console.log(a);
 console.log(b);
 
+//returning the maximum of two numbers
 function max(num1, num2) {
     if (num1 > num2)
         return num1;
     else
         return num2;
+    console.log(max(3,5));
+}
+//returning the maximum of two numbers (shorter method)
+function maxnum(a, b) {
+    return (a>b) ? a : b;
 }
